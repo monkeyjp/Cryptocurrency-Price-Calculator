e2b178a6765a14fbae8b04e759176160da6321da
Javier Pestana <javier.pestana85@gmail.com>
2024-09-19 09:54:44 +0100

    Setup vue app and first form for currency select

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const currencies = ref([
+  { code: "USD", text: "United States Dollar" },
+  { code: "MXN", text: "Mexican Peso" },
+  { code: "EUR", text: "Euro" },
+  { code: "GBP", text: "Pound" },
+]);
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1 class="titulo">Cryptocurrency <span>Price Calculator</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="currency">Currency:</label>
+          <select id="currency">
+            <option value="">-- Select --</option>
+            <option v-for="currency in currencies" :value="currency.code">
+              {{ currency.text }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
